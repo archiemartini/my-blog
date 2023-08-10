de84e269c14b6d8401ed639272171d30bddfc067
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2023-08-10 16:47:50 +0100

    added axios and fetched comment info

---
+++ src/components/CommentsList.tsx
@@ -0,0 +1,16 @@
+export interface Comment {
+  postedBy: string;
+  text: string
+}
+
+export const CommentsList = ({ comments }: { comments: Comment[]}) => (
+  <>
+  <h3>Comments:</h3>
+  {comments.map(comment => (
+    <div className="comment" key={comment.postedBy + ': ' + comment.text}>
+      <h4>{comment.postedBy}</h4>
+      <p>{comment.text}</p>
+    </div>
+    ))}
+  </>
+)
--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,19 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import {Article, articles} from './article-content'
 import NotFoundPage from './NotFoundPage'
+import { CommentsList } from '../components/CommentsList'
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
   const { articleId } = useParams()
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`)
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo)
+    }
+
+    loadArticleInfo()
+  }, [articleId])
+
   const article: Article | undefined = articles.find(article => article.name === articleId)
+
   if (!article) {
     return <NotFoundPage />
   }
+
   return (
     <>
     <h1>{article.title}</h1>
+    <p>This article has {articleInfo.upvotes} upvote(s)</p>
     {article.content.map(paragraph => (
       <p>{paragraph}</p>
     ))}
+    <CommentsList comments={articleInfo.comments} />
     </>
   );
 }
